a94a6df8499c74f5b780726a81cb62a2fbb19f3f
fab <fabrizio.fiandanese@gmail.com>
2021-10-19 15:12:07 +0200

    add types and Pretty instances

--- src/Lib/PulseAudio/Types.hs
+++ src/Lib/PulseAudio/Types.hs
@@ -1,14 +1,28 @@
+{-# LANGUAGE RecordWildCards #-}
 module Lib.PulseAudio.Types where
 
-import Data.Text
+import           Data.Text
+import           Prettyprinter
 
-data SinkStatus = SinkRunning | SinkSuspended deriving (Show,Eq,Ord)
+data SinkStatus = SinkRunning | SinkSuspended | SinkIdle | SinkStatusUnknown deriving (Show,Eq,Ord)
 
 data Sink = Sink {
-                    sinkId:: !Int,
-                    sinkName :: !Text,
-                    sinkLib :: !Text,
-                    sinkBits :: !Text,
-                    sinkFreq :: !Text,
-                    sinkStatus :: !SinkStatus 
+                    sinkId         :: !Int,
+                    sinkName       :: !Text,
+                    sinkDriver     :: !Text,
+                    sinkSampleSpec :: !Text,
+                    sinkStatus     :: !SinkStatus
                  } deriving (Show,Eq,Ord)
+
+--- PrettyPrinter
+
+instance Pretty SinkStatus where
+        pretty status =
+                case status of
+                  SinkIdle      -> pretty "Idle"
+                  SinkRunning   -> pretty "Running"
+                  SinkSuspended -> pretty "Suspended"
+                  _             -> pretty "n/a"
+
+instance Pretty Sink where
+        pretty Sink{..} = brackets (pretty sinkId) <+> pretty sinkName <+> parens (pretty sinkStatus)
