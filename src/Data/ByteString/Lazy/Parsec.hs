32850da9664d55cb6a3b43c55be316539ba2a11d
fab <fabrizio.fiandanese@gmail.com>
2021-10-19 15:13:33 +0200

    add Parsec utils and parsers

---
+++ src/Data/ByteString/Lazy/Parsec.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Data.ByteString.Lazy.Parsec (module Data.ByteString.Lazy.Parsec, module Text.Parsec.ByteString.Lazy, module Text.Parsec) where
+
+import           Control.Applicative         ((<*))
+import           Control.Monad               (void)
+import           Data.ByteString.Lazy        (ByteString (..))
+import           Data.ByteString.Lazy.UTF8   (fromString)
+import           Data.Char
+import           Data.Text                   (Text (..), pack)
+import           Text.Parsec                 hiding (parse)
+import qualified Text.Parsec
+import           Text.Parsec.ByteString.Lazy
+
+-- helpers
+
+parse :: Parser a -> ByteString -> Either ParseError a
+parse p = Text.Parsec.parse p ""
+
+parseWithEof :: Parser a -> ByteString -> Either ParseError a
+parseWithEof p = Text.Parsec.parse (p <* eof) ""
+
+parseWithLeftOver :: Parser a -> ByteString -> Either ParseError (a, String)
+parseWithLeftOver p = Text.Parsec.parse ((,) <$> p <*> leftOver) ""
+                       where leftOver = manyTill anyToken eof
+
+tabChar :: Parser Char
+tabChar = satisfy (== '\t')
+
+notTabChar :: Parser Char
+notTabChar = satisfy (/= '\t')
+
+isNotTabChar :: Char -> Bool
+isNotTabChar = (/= '\t')
+
+allButTabChar :: Parser Text
+allButTabChar = do
+                 chars <- many1 $ satisfy isNotTabChar
+                 pure $ Data.Text.pack chars
+
+digitField :: Parser Int
+digitField = do
+               num <- many1 $ satisfy (\x -> isDigit x && isNotTabChar x)
+               pure $ read num
+
+boolField :: Parser Bool
+boolField = (== "yes") <$> (string "yes" <|> string "no")
+
+whitespace :: Parser ()
+whitespace = void $ many $ oneOf " \n\t"
+
+trimWSstart :: Parser a -> Parser a
+trimWSstart parser = whitespace *> parser
+
+trimWStartAndEnd :: Parser a -> Parser a
+trimWStartAndEnd parser = whitespace *> parser <* whitespace
+
+-- note how this and tabbedField below are identical!
+tabbedFieldParser :: Parser a -> Parser a
+tabbedFieldParser fieldTypeParser = do
+                   field <- trimWStartAndEnd fieldTypeParser
+                   void $ optional tabChar
+                   pure field
+
+tabbedField :: Parser a -> Parser a
+tabbedField p = trimWStartAndEnd p <* optional tabChar
+
+tabbedTextField :: Parser Text
+tabbedTextField = tabbedField allButTabChar
+
+tabbedDigitField :: Parser Int
+tabbedDigitField = tabbedField digitField
+
+-------
+
+-- personParser :: Parser P
+-- personParser = P
+--                <$> csvField (exactMatchField <|> alphaNumField)
+--                <*> csvField (exactMatchField <|> alphaNumField)
+--                <*> csvField digitField
+--                <*> csvField boolField
+--                <*> csvField monthField
+
+-- input = " fab , fiandanese ,42, yes, 11-2020"
+-- examples = do
+--     parseWithEof personParser input
