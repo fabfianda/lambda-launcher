a9533c1465490a89e89e96ab771e31c1b00e96da
fab <fabrizio.fiandanese@gmail.com>
2021-10-19 15:12:29 +0200

    refactor: API

--- src/Lib/PulseAudio/Ctl.hs
+++ src/Lib/PulseAudio/Ctl.hs
@@ -1,27 +1,55 @@
-{-# LANGUAGE TemplateHaskell      #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
 {-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE TemplateHaskell      #-}
 
 module Lib.PulseAudio.Ctl where
 
+import qualified Data.ByteString.Lazy        as BSL (ByteString)
+import           Data.ByteString.Lazy.Parsec
+import           Data.ByteString.Lazy.Utils
+import           Data.Functor
+import           Lib.PulseAudio.Types
 import           Shh
-import qualified Data.ByteString.Lazy as BSL(ByteString())
-import Data.ByteString.Lazy.Utils
-import Data.Functor
 
 $(load SearchPath["pactl"])
 
--- API
+-- Shell
 
-getShortSinksList :: Command t => t
-getShortSinksList = pactl "list" "short" "sinks"
+listShortSinks :: Command t => t
+listShortSinks = pactl "list" "short" "sinks"
+
+-- API
+getSinksList :: (Shell f, Monad f) => f (Either ParseError [Sink])
+getSinksList = do
+        sinks <- listShortSinks |> captureLines
+        let eSinks = sinks <&> toSinkType
+        pure $ sequence eSinks
+
+-- Parser
+sinkParser :: Parser Sink
+sinkParser = Sink <$> tabbedDigitField
+                  <*> tabbedTextField
+                  <*> tabbedTextField
+                  <*> tabbedTextField
+                  <*> tabbedField sinkStatusParser
+
+sinkStatusParser :: Parser SinkStatus
+sinkStatusParser = do
+        match <- string "SUSPENDED" 
+             <|> string "IDLE"
+             <|> string "RUNNING"
+        pure $ case match of
+          "SUSPENDED" -> SinkSuspended
+          "IDLE"      -> SinkIdle
+          "RUNNING"   -> SinkRunning
+          _           -> SinkStatusUnknown
+
+-- boolField :: Parser Bool
+-- boolField = do
+--              output <- string "yes" <|> string "no"
+--              pure $ output == "yes"
+--
 
 -- Conversions
 
-toSinkType = splitTabs
-
--- 
-sinksList :: (Shell f, Monad f) => f [[BSL.ByteString]]
-sinksList = do 
-        sinks <- getShortSinksList |> captureLines
-        pure $ sinks <&> toSinkType
+toSinkType = parse sinkParser
