c45cdcf0b317036828ee5764a0e883f9cd25409a
fab <fabrizio.fiandanese@gmail.com>
2021-10-21 08:19:25 +0200

    improve Dmenu option

--- src/Lib/Rofi.hs
+++ src/Lib/Rofi.hs
@@ -9,17 +9,17 @@ import           Control.Exception         (SomeException, catch)
 import           Data.ByteString.Lazy.UTF8 (ByteString, fromString)
 import           Data.Functor              ((<&>))
 import           Data.List                 (elemIndex, (!!))
+import           Data.Text
 import           Prettyprinter             (Pretty (pretty))
 import           Shh                       (ExecReference (SearchPath),
                                             captureTrim, load, (|>))
 import qualified Shh.Internal              as Shh (unlines)
-import           System.Environment        ()
 
 $(load SearchPath["rofi", "ls", "echo"])
 
 -- Shell
 
-rofiDmenu = rofi "-dmenu"
+rofiDmenu title = rofi "-dmenu" "-p" (fromString title)
 
 -- Types
 type Renderer a = (a -> ByteString)
@@ -32,15 +32,15 @@ basicRenderer :: Show a => a -> ByteString
 basicRenderer = fromString . show
 
 -- API
-pickItem :: Renderer a -> [a] -> IO ( Maybe a )
-pickItem render items = do
+pickItem :: String -> Renderer a -> [a] -> IO ( Maybe a )
+pickItem title render items = do
         let renderedItems = items <&> render
-        item <- catch( echo (Shh.unlines renderedItems ) |> rofiDmenu |> captureTrim )
+        item <- catch( echo (Shh.unlines renderedItems ) |> rofiDmenu title |> captureTrim )
                      ( \(e :: SomeException) -> pure "" )
 
         return $ case elemIndex item renderedItems of
           Just index -> Just $ items !! index
           Nothing    -> Nothing
 
-prettyPickItem :: (Pretty a) => [a] -> IO (Maybe a)
-prettyPickItem = pickItem prettyRenderer
+prettyPickItem :: (Pretty a) => String -> [a] -> IO (Maybe a)
+prettyPickItem title = pickItem title prettyRenderer
