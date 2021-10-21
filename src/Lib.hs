0f96872e29cdc94aa7c07f54f580b277f05bdeec
fab <fabrizio.fiandanese@gmail.com>
2021-10-21 08:19:37 +0200

    inprogress

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,8 +7,10 @@ module Lib
     ) where
 
 import           Lib.PulseAudio.Ctl
+import Data.Functor
 import           Lib.Rofi
 import           Shh
+import Data.Either
 
 
 main :: IO ()
@@ -17,12 +19,28 @@ main = do
    eSinks <- getSinksList
    case eSinks of
      Right sinks -> do
-        sink <- prettyPickItem sinks
+        sink <- prettyPickItem "Sinks" sinks
         case sink of
           Just sink -> do
                        vol <- getSinkVolume sink
-                       print vol
+                       case vol of
+                         Right vol -> do
+                                 newVol <- prettyPickItem ("Volume [" ++ show vol ++ "]") (ramp 5 0 100)
+                                 case newVol of
+                                   Just newVol -> fadeSinkVolume 5 vol newVol sink
+                                   Nothing     -> print "nothing to do here"
+
+                         Left e -> print "Volume fetch error"
+
           _         -> print "nothing to see here"
 
 
      Left e -> print e
+
+-- alt :: IO ()
+-- alt = do
+--    initInteractive
+--    sink <- getSinksList >>= (\eSinks -> do 
+--                                          sink <- prettyPickItem "Sinks" eSinks
+--                                          pure sink
+--                             )
